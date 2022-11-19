68fdbc687c042b8ee39e622ac7cd24b121daa9eb
atcamo <atiliocm@gmail.com>
2022-11-18 22:38:17 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+/*crear variable*/ 
+var myName;
+/*asignar valor variable*/ 
+var a;
+a = 7;
+/*asignar de una variable a otra variable*/ 
+var b;
+b = a;
+/*crear variable asignar valor*/ 
+var myVar = 0;
+/*crear variable asignar valor*/
+var a = 9;
+/*crear variable asignar cadena (string)*/
+var myFirstName = "Atilio";
+var myLastName = "Calderon";
+/*incrementa valor*/
+i = i + 1 ;
+i++ ;
+/*decrementa valor*/
+i = i - 1 ;
+i-- ;
+/*multiplica valor*/
+i = i * 1 ;
+i *= 1 ;
+/*divide valor*/
+i = i / 1 ;
+i/= 1;
+
+/*escape secuencia
+\' comilla simple 
+\" comilla doble
+\\ barra invertida
+\n linea nueva 
+\t tabulador 
+\r retorno de carro 
+\b limite de palabra
+\f fuente de formulario */
+
+/*concatenar cadena*/
+let ourStr = "i come first";
+ourStr ="I come second";
